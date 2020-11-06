c5fe7c9e33f7ac2fa014eecb56488ea89098a920
Ege Aydın <egemb@hotmail.com>
2020-11-06 16:46:01 +0300

    Proje dosyası ekle.

---
+++ Faktoriyel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktoriyel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı giriniz : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int a= 1;
+            
+            if (sayi <= 1 && sayi != 0)
+            {
+                Console.WriteLine("Lütfen bir sayı giriniz");
+            }
+            else
+            {
+                for (int i = 1; i < sayi; i++)
+                {
+                    a += a * i;
+                }
+                Console.WriteLine( sayi + " sayısının faktoriyel hesabı :" + a);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
